4b80f1b028ed510f75495d169511f214078ee171
Alex Benoit <alexander.benoit@outlook.com>
2023-03-19 08:52:45 -0700

    Typo escape html

--- lib/vanilla_nested/view_helpers.rb
+++ lib/vanilla_nested/view_helpers.rb
@@ -30,7 +30,7 @@ module VanillaNested
       classes = "vanilla-nested-add #{link_classes}"
       data = {
         'container-selector': container_selector,
-        'html': escape_once html,
+        'html': escape_once(html),
         'method-for-insert': method_for_insert
       }
 
